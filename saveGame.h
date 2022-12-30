78d507511968ba9d9449e3f1cd2615ddaf043e37
Ahmad Hassan <43483116+MidoHassan@users.noreply.github.com>
2022-12-30 20:54:12 +0200

    Delete game

--- saveGame.h
+++ saveGame.h
@@ -13,7 +13,7 @@ char* parseGameName(char* data);
 
 int SaveBoard(struct Board* B, char* filename, char* gameprename);///Example:SaveBoard(&B,"file_to_save_structs.txt","game1")
 
-
+int DeleteBoard( char* filename, char* gameprename);
 int LoadBoard(struct Board* B, char* filename, char* gameprename);
 
 int LoadGame(struct Board* B, struct Player* P1, struct Player* P2, int* turns, char* filename, char* gameprename);///Example:LoadGame(&B,&P1,&P2,&turns,"file_to_save_structs.txt","game1")
